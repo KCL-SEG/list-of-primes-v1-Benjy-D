f209bac010b2f928de2a1bf9f1f861faec259050
Benjamin Donoff <benjamin@donoff.com>
2023-09-27 17:28:38 +0100

    Task Completed

--- primes.py
+++ primes.py
@@ -3,4 +3,24 @@
 
 def primes(number_of_primes):
     list = []
+    """for num in range(2, number_of_primes + 2):
+          for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            list.append(num)
+            """
+    count = 1
+    while len(list) < number_of_primes:
+        count += 1
+        isPrime = True
+        for num in range(2, count):
+            if count % num == 0:
+                isPrime = False
+                break
+
+        if isPrime:
+            list.append(count)
+
+
     return list
